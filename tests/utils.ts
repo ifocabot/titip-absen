87c64c93200671262849e34fffe56603f8fefab4
ifocabot <77793836+ifocabot@users.noreply.github.com>
2024-07-28 23:10:57 +0700

    Update utils.ts

--- tests/utils.ts
+++ tests/utils.ts
@@ -22,9 +22,8 @@ export async function goToLiveAttendance(page: Page) {
 
   await page.getByRole("button", { name: "Sign in", exact: true }).click();
 
-  // attendance page
-  await page.goto("https://hr.talenta.co");
-  await page.getByText("Live attendance", { exact: true }).click();
+  // langsung ke halaman Live Attendance
+  await page.goto("https://hr.talenta.co/live-attendance");
 }
 
 export function skip() {
